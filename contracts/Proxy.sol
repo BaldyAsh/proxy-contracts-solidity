46684f3d79605cea30236e82a6ddd67cff421f63
BaldyAsh <antongrigorjev2010@gmail.com>
2020-02-03 10:10:07 +0300

    removed truffle, fixed waffle, added test contracts

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,22 +1,20 @@
-pragma solidity 0.6.2;
+pragma solidity 0.5.16;
 
 import "./Upgradeable.sol";
 
-/// @title Proxy Contract
+
+/// @title Upgradeable Proxy Contract
 /// @author Anton Grigorev (@BaldyAsh)
 contract Proxy is Upgradeable {
     /// @notice Contract constructor
     /// @dev Calls Upgradable contract constructor
-    constructor() Upgradeable() public {}
+    constructor() public Upgradeable() {}
 
-    /// @notice Performs a delegatecall to the contract implementation
-    /// @dev Fallback function allowing to perform a delegatecall to the given implementation.
+    /// @notice Performs a delegatecall to the implementation contract.
+    /// @dev Fallback function allows to perform a delegatecall to the given implementation.
     /// This function will return whatever the implementation call returns
-    receive() external payable {
-        require(
-            msg.data.length > 0,
-            "9d96e2df01"
-        ); // 9d96e2df01 - calldata must not be empty
+    function() external payable {
+        require(msg.data.length > 0, "9d96e2df01"); // 9d96e2df01 - calldata must not be empty
         address _impl = getImplementation();
         assembly {
             // The pointer to the free memory slot
@@ -24,28 +22,21 @@ contract Proxy is Upgradeable {
             // Copy function signature and arguments from calldata at zero position into memory at pointer position
             calldatacopy(ptr, 0x0, calldatasize)
             // Delegatecall method of the implementation contract, returns 0 on error
-            let result := delegatecall(
-                gas,
-                _impl,
-                ptr,
-                calldatasize,
-                0x0,
-                0
-            )
+            let result := delegatecall(gas, _impl, ptr, calldatasize, 0x0, 0)
             // Get the size of the last return data
             let size := returndatasize
             // Copy the size length of bytes from return data at zero position to pointer position
             returndatacopy(ptr, 0x0, size)
             // Depending on result value
             switch result
-            case 0 {
-                // End execution and revert state changes
-                revert(ptr, size)
-            }
-            default {
-                // Return data with length of size at pointers position
-                return (ptr, size)
-            }
+                case 0 {
+                    // End execution and revert state changes
+                    revert(ptr, size)
+                }
+                default {
+                    // Return data with length of size at pointers position
+                    return(ptr, size)
+                }
         }
     }
 }
